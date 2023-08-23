5a2d09d488c323e64984450ec50ca7d21c4c4105
Joao Vitor <joaovitor19.03.2004@gmail.com>
2023-08-23 13:58:57 -0300

    mudancas

--- questao1.py
+++ questao1.py
@@ -1,6 +1,6 @@
 # Faça um programa, utilizando while, que mostre na tela
 # os números de 0 a 100.
-print("Números exibidos de 0  a 100:")
+print("\033[032mNúmeros exibidos de 0  a 100:\033[m")
 numeros = 0
 while numeros <= 100:
     print(numeros)
--- questao3.py
+++ questao3.py
@@ -1,12 +1,12 @@
 # Faça um programa, utilizando while, 
 # que permita o usuário fazer contas de adição enquanto quiser.
-print("Operação - Adição!")
+print("Operação - \033[032mAdição!")
 continuar = True
 while continuar:
-    num1 = int(input("\nDigite um número:"))
-    num2 = int(input("Digite outro número:"))
+    num1 = int(input("\n\033[033mDigite um número:\033[m"))
+    num2 = int(input("\033[033mDigite outro número:\033[m"))
     soma = num1 + num2
-    print(f'\n{num1}+{num2}={soma}\n')
-    resposta = input("Deseja realizar mais uma soma?[S ou N]\nResposta:")
+    print(f'\n{num1}\033[032m + \033[m{num2} = \033[032m{soma}\n')
+    resposta = input("\033[033mDeseja realizar mais uma soma?[S ou N]\nResposta:\033[m")
     if resposta.lower()!= "s":
         continuar = False 
