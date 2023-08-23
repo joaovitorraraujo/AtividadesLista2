65e37b618389cfbe9e6b52595764259a149defd8
Joao Vitor <joaovitor19.03.2004@gmail.com>
2023-08-23 09:30:58 -0300

    .

--- qustao 2.py
+++
@@ -1,7 +0,0 @@
-# Faça um programa, utilizando while, que mostre na tela 
-# de 0 até N, em que N é o limite inserido pelo usuário.
-numeros = 0
-limite = int(input("Informe um número limite:"))
-while numeros <= limite:
-    print(numeros)
-    numeros+=1
